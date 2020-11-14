39bad83fc19119a611959f88e66cd2d003eec210
YauheniPo <ip.popovich@mail.ru>
2020-11-14 13:18:00 +0300

    Added swiping custom func by elements

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Appium_Swipe_Element
+
+## Pip can install packages from Git
+I have hosted a Python package on GitHub (python_world), which you can install with the following command (make sure you trust me before running this command and installing my code on your computer).
+
+pip install git+https://github.com/YauheniPo/Appium_Swipe_Element.git#egg=Appium_Swipe_Element
+
+Pip provides options to install from head, from a branch, from a tag or from a commit. I usually tag each release and install from these tags. See the pip install documentation for full details.
--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
+import setuptools
 from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='appium_swipe_element',
@@ -7,7 +11,12 @@ setup(
     url='https://github.com/YauheniPo/Appium_Swipe_Element.git',
     author='Yauheni Papovich',
     author_email='ip.popovich.1990@gmail.com',
-    license='unlicense',
-    packages=['appium_swipe_element'],
-    zip_safe=False
+    license='MIT',
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
